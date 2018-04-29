dbc92d5bd9e21c3bc11f39944ae88ad6b41fac66
Pavithra Balasubramaniam <pavis.balu@gmail.com>
2018-04-29 21:44:45 +0530

    Making service port configurable

--- src/main/java/in/ashwanthkumar/restvideos/module2/KeyValueService.java
+++ src/main/java/in/ashwanthkumar/restvideos/module2/KeyValueService.java
@@ -1,11 +1,24 @@
 package in.ashwanthkumar.restvideos.module2;
 
 import io.dropwizard.Application;
+import io.dropwizard.configuration.EnvironmentVariableSubstitutor;
+import io.dropwizard.configuration.SubstitutingSourceProvider;
+import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
 
 public class KeyValueService extends Application<ServiceConfiguration> {
     public static void main(String[] args) throws Exception {
         new KeyValueService().run(args);
+    }
+
+    @Override
+    public void initialize(Bootstrap<ServiceConfiguration> bootstrap) {
+        super.initialize(bootstrap);
+        bootstrap.setConfigurationSourceProvider(
+                new SubstitutingSourceProvider(bootstrap.getConfigurationSourceProvider(),
+                        new EnvironmentVariableSubstitutor(false)
+                )
+        );
     }
 
     @Override
